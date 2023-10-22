aebaaaeb043a568bd252a814fd9726aa6e1e54e5
midbar pakar <pakpakpkar@gmail.com>
2023-10-22 04:11:44 +0300

    hey

--- Main/my_whatsapp_bot.js
+++ Main/my_whatsapp_bot.js
@@ -1,9 +1,26 @@
 try {
+    const {
+        usersMessage,
+        resetMessage,
+        permissionsMessage,
+        loginMessage,
+        dashboardMessage,
+        kfgMessage,
+        dataMessage,
+        arkMessage,
+        WelcomeMessage
+    } = require('./messages');
+
     const { Client } = require('whatsapp-web.js');
+
     const qrcode = require('qrcode');
+
     const client = new Client();
 
     const receivedOneTimeMessage = new Set();
+
+    const pendingIssues = {};
+    const pendingIssuesForData = {};
 
     client.on('authenticated', (session) => {
         console.log('Authenticated:', session);
@@ -22,90 +39,10 @@ try {
         console.log('Client is ready!');
     });
 
-    const WelcomeMessage = `תודה שפניתם לחמ''ל אגמים, על מנת לקבל את המענה הרצוי, שלחו את המספר הרלוונטי.
-
-    לטיפול *ביצירת משתמשים* - שלחו *1*
-
-    לטיפול *באיפוס סיסמה* - שלחו *2*
-
-    לבקשת הרשאות וטיפול בבעיות *הרשאות* (ArcGIS/PowerBI) - שלחו *3*
-
-    לטיפול בקשיים *בהתחברות* - שלחו *4*
-
-    להסבר על *דשבורדים* - שלחו *5*
-
-    לטיפול בקשיים במערכת אגמים *בכפר הגלובלי* - שלחו *6*
-
-    לבקשת *תוצרים/נתונים* - שלחו *7*
-
-    לטיפול בבעיות *בתיבת נוח*/בקשת העברת מלונות - שלחו *8*
-
-    לשיחה עם *נציג אנושי* - שלחו *9*
-
-    לשליחה חוזרת של *הודעה זו*, שלחו *0*
-
-    * *יש לציין שפתיחת משתמש כרוכה באישור קצין אגמים*
-
-    * *שליחת הספרה הרלוונטית תציג הסבר על בעיות נפוצות בנושא הנבחר/תטפל בנושא הנבחר ותוכל להדריך אתכם עד לפתירת הבעיה.*
-
-    * *חמ''ל אגמים זמין 24/7 לרשותכם.*`
-
-    const usersMessage = `לצורך פתיחת משתמש אגמי"ם יש למלא את הסקר הבא:
-https://ly.agamimp.net/requestuser
-
-*שימו לב, פתיחת משתמש מותנת באישור קצין אגמי"ם, יש לוודא עם אישור הקשר שלכם מהענף לפני הגשת הטופס*`
-
-    const resetMessage = `לפני איפוס הסיסמה נשמח שתבדקו האם נכנסתם דרך מסך כניסה של פיקוד העורף או דרך השם משתמש וסיסמה המופיעים בעת לחיצה על הלינק לפורטל.
-
-במידה ונכנסתם דרך השם משתמש וסיסמה, אנא שלחו 4
-
-במידה וסיסמתכם אינה נכונה גם במסך הכניסה של פיקוד העורף, אנא שלחו את האימייל הנדרש לאיפוס הסיסמה, והבעיה תטופל בהקדם.`
-
-    const permissionsMessage = `במידה ונתקלת במסך לבן עם שתי כפתורים והצהרה שאינך יכול לצפות במידע הזה, אנא שלח את האימייל שלך.
-
-במידה ונתקלת במסך לבן עם כיתוב דומה ללא שתי כפתורים, אנא פנה לנציג אנושי עם פרטיך הצבאיים (תפקיד, שם ומחלקה).
-
-במידה והינך מנסה לצפות בדשבורד BI מחוץ לפורטל, אנא פנה לנציג אנושי עם פרטיך הצבאיים (תפקיד, שם ומחלקה).`
-
-    const loginMessage = `במידה והנכם חווים תקלות בהתחברות בכפר הגלובלי, אנא שלחו 6.
-
- והנכם מנסים להתחבר עם משתמש agamimp.net, אנא וודאו שאתם נמצאים בכניסת משתמש פיקוד העורף.
-
-במידה ואתם בדף התחברות המציג שם משתמש וסיסמה, ומתחתיו URL ארגוני (Organization), לחצו על הURL הארגוני, והזינו H-F-C, יפתח לכם דף התחברות חדש עם כפתור כחול, הרשום בו HFC MAPS, לחצו עליו ותגיעו לדף ההתחברות הנכון, שם תזינו את משתמש Agamimp.net שלכם, ואת סיסמתכם, במידה ואינכם זוכרים את הסיסמה, שלחו 2.
-
-במידה ואתם בדף התחברות המציג כבר את הכפתור הכחול של הHFC MAPS, אנא לחצו עליו, ותגיעו לדף ההתחברות הנכון.
-
-לתקלה שלא כתובה כאן, אנא שלחו *9* ולאחר מכן, צרפו לפנייתכם צילום מסך.`
-    
-    const dashboardMessage = `הסבר על דשבורדים ניתן להשיג בשלושה דרכים.
-
-הדרך הראשונה היא שליחת *9* לשוחח עם נציג, שיסביר לכם על הדשבורדים הרלוונטים לכם.
-
-הדרך השניה היא להיכנס לפורטל חרבות הברזל, להיכנס שם לסרטוני ההדרכה, ולצפות בסרטון המסביר על הדשבורד הרלוונטי לכם.
-
-הדרך השלישית היא להתקשר לחמ''ל אגמים, ולקבל הסבר טלפוני על הדשבורדים הרלוונטים לכם.`
-
-    const kfgMessage = `במקרה של תקלת אי טעינת כלל הדשבורדים או הפורטל בכפר הגלובלי, יש לפתוח את כרטיסיית הפורטל בלשונית חדשה.
-
-במידה ועדיין לא נפתח הפורטל, יש לעשות ריסטרט למחשב, דבר זה יפתור את הבעיה.
-
-במידה והsign in לדשבורדי Power BI לא עובד, יש ללחוץ על הSign In, דבר אשר יפתח ויסגור כרטיסיה, ולאחר מכן, לפתוח את הפורטל בכרטיסיה חדשה.
-
-במידה ומוצגת לכם שגיאה אינטרנטית, יש לפנות לתקשוב, במחשבי כפר גלובלי  מסוימים יש הגבלות ליכולות של PowerBI ותקלה זו אינה נובעת מאגמים.`
- 
-    const dataMessage = `לבקשת תוצרים/נתונים יש להזין את הפרטים הבאים בצורה מסודרת:
-שם מלא
-תפקיד
-נתון/תוצר רצוי
-צורך בתוצר
-האם יש לכם גורם מקשר באגמים (קצין).
-
-לאחר מכן נציג יפנה אליכם לטיפול בבקשה/לפרטים נוספים
-
-* *נתוני מובי סלולר לא זמינים כרגע ליצוא ומוצגים אך ורק בתוצרים*
-* *דשבורד דו''ח קהא כרגע לא באוויר והוא בשלבי סיום נכון לכתיבת הודעה זו, התאזרו בסבלנות*`
-
-    const arkMessage = `מחכה לאיתי`
+    const targetIDs = {
+        '9': '120363194824840486@g.us',
+        '7': '120363192179109469@g.us',
+    };
 
     const messages = {
         '1': usersMessage,
@@ -115,22 +52,89 @@ https://ly.agamimp.net/requestuser
         '5': dashboardMessage,
         '6': kfgMessage,
         '7': dataMessage,
-        '8' : arkMessage,
-        '9': 'מיד יפנה אליכם אחד מנציגינו',
+        '8': arkMessage,
         '0': WelcomeMessage,
     }
 
 
     client.on('message', async message => {
         try {
+
+            if (message.from === "120363194824840486@g.us" && message.hasQuotedMsg) {
+          
+                const quotedMsg = await message.getQuotedMessage();         
+             
+                const match = quotedMsg.body.match(/Issue from (\d+):/);
+           
+                if (match) {
+            
+                    const originalSender = `${match[1]}@c.us`;         
+            
+                    client.sendMessage(originalSender, `${message.body}`);
+                }      
+                return;
+            }
+
+            
+            if (message.from === "120363192179109469@g.us" && message.hasQuotedMsg) {
+            
+                const quotedMsg2 = await message.getQuotedMessage();
+              
+                const match2 = quotedMsg2.body.match(/Issue from (\d+):/);
+             
+                if (match2) {
+              
+                    const originalSender2 = `${match2[1]}@c.us`;
+            
+                    client.sendMessage(originalSender2, `${message.body}`);
+                }
+                return;
+            }
+
             if (message.from.length > 17) {
                 return;
             }
+
+        if (pendingIssues[message.from]) {
+            const targetID = "120363194824840486@g.us";
+
+            const phoneNumber = message.from.split('@')[0];
+
+            client.sendMessage(targetID, `Issue from ${phoneNumber}:
+            
+            ${message.body}`);
+            delete pendingIssues[message.from]; 
+            return;
+        }
+
+        if (pendingIssuesForData[message.from]) {
+            const targetIDforData = "120363192179109469@g.us";
+
+            const phoneNumber2 = message.from.split('@')[0];
+
+            client.sendMessage(targetIDforData, `Issue from ${phoneNumber2}:
+            
+            ${message.body}`);
+            delete pendingIssuesForData[message.from]; 
+            return;
+        }
 
             if (!receivedOneTimeMessage.has(message.from)) {
                 await message.reply(WelcomeMessage); 
                 receivedOneTimeMessage.add(message.from); 
             }
+
+        if (message.body === '9') {
+            await message.reply("אנא פרט את בעייתך ככל האפשר בהודעה אחת, פנייתך תועבר לנציג אנושי");
+            pendingIssues[message.from] = true;  
+            return;
+        }
+
+        if (message.body === '7') {
+            await message.reply(dataMessage);
+            pendingIssuesForData[message.from] = true;  
+            return;
+        }
 
             if (!Object.keys(messages)[message.body]) {
                 return;
